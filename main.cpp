f7ba6f5690504989db68bb18025c79b02a4b488b
dengpeterater <dengpeterater@gmail.com>
2026-05-14 00:19:34 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,31 +1,29 @@
 #include <iostream>
-#include <vector>
+
 using namespace std;
 
-int main() {
-    int chosenNumber = 7;
-    int userGuess = 0;
-    vector<int> guesses;
+int main()
+{
+    int arr[4]={16,28,9,10};
 
-    while (userGuess != chosenNumber) {
-        cout << "Enter your guess: ";
-        cin >> userGuess;
-
-        guesses.push_back(userGuess);
-
-        if (userGuess > chosenNumber) {
-            cout << "Too high" << endl;
-        } else if (userGuess < chosenNumber) {
-            cout << "Too low" << endl;
-        } else {
-            cout << "Correct!" << endl;
+    int n=4;
+    cout<<"before sorting"<<endl;
+    for(int i=0;i<4;i++){
+        cout<<arr[i]<<endl;
+    }
+       cout<<"after swapping"<<endl;
+       for(int i=0;i<4;i++){
+        for(int j=0;j<4;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j]=arr[j+1];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+            }
         }
-    }
+          for(int i=0;i<4;i++){
+            cout<<arr[i]<<endl;
+            return 0;
 
-    cout << "\nAll guesses:" << endl;
-    for (int i = 0; i < guesses.size(); i++) {
-        cout << guesses[i] << endl;
-    }
-
-    return 0;
-}
+          }
+       }
