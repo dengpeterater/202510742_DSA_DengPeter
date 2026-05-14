1710a98f8a2fd47ab78f70b7940327df1196140f
dengpeterater <dengpeterater@gmail.com>
2026-05-14 23:36:39 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
-int add(int a, int b); // declaration
+#include <stdio.h>
 
-int add(int a, int b) { // definition
-    return a + b;
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;   // base case
+    return n * factorial(n - 1); // recursive case
 }
 
 int main() {
-    int result = add(2, 3); // function call
+    printf("Factorial = %d\n", factorial(5));
+    return 0;
 }
